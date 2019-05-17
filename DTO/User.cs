809543deab1ae34d1a4e76f0679d11fcbc7a2db4
David <david.felley@students.hevs.ch>
2019-05-17 14:50:47 +0200

    Création DTO et objet User

---
+++ DTO/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Societe { get; set; }
+        public string Adresse { get; set; }
+        public string Telephone { get; set; }
+        public string Mail { get; set; }
+        public string Descritpion { get; set; }
+        public string IdCompetence { get; set; }
+        public int IdActivite { get; set; }
+        public int IdLocalisation { get; set; }
+        public int IdCategoryUser { get; set; }
+
+    }
+}
